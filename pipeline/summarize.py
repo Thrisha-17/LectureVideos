7ef1e53b1aeab30905ae255b01130eccd3bb316b
Thrisha D <thrishaduraisamy@gmail.com>
2026-05-18 16:20:16 +0530

    Delete pipeline directory

--- pipeline/summarize.py
+++
@@ -1,7 +0,0 @@
-from transformers import pipeline
-
-summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-
-def summarize_text(text: str):
-    summary = summarizer(text, max_length=150, min_length=40, do_sample=False)
-    return summary[0]['summary_text']
--- pipeline/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def ensure_dir(path):
-    if not os.path.exists(path):
-        os.makedirs(path, exist_ok=True)
-
-def save_text(path, text):
-    with open(path, "w", encoding="utf-8") as f:
-        f.write(text)
